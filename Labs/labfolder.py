14a59bef3a9c16e1b3c55d761dff78b6da90e6fa
Don-Antonio18 <antoniokerrrocks@gmail.com>
2024-09-28 14:25:41 -0500

    lol

--- Labs/labfolder.py
+++ Labs/labfolder.py
@@ -1,1 +1,8 @@
+def recursion(x):
+    if x == 1:
+        return 1
+    else:
+        return recursion(x -1) * x
 
+number = int(input("\nenter a number to get its factorial: "))
+print (recursion(number))
