1f9cd9c08df06ca91caa08d9f10c6c469d616d34
Don-Antonio18 <antoniokerrrocks@gmail.com>
2024-09-28 03:19:24 -0500

    Update

--- factors.py
+++ factors.py
@@ -1,15 +1,14 @@
 
 '''this program determines which numbers in the list
-        can be DIVIDED INTO X without a remainder, aka  [ % == 0 ] '''
+        can be DIVIDED  INTO X without a remainder, aka  [ % == 0 ] '''
 import time
 def factors(x):  
-    #x = 8
-    #for n in [1,2,3,4,5,6,7,8,9,10]:
     for n in range(1,x+1):              
         if x % n == 0 :
             print ('\n Factor: ', n)
             time.sleep(0.1)
     print("End")
+    print()
 
 num = int(input("Enter a number to see its factors: "))
 factors(num)
