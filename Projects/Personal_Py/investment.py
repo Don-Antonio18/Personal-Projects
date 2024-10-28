1eb887588ae955b739c52db41f6116e2e0da1b6b
Don-Antonio18 <antoniokerruni@gmail.com>
2024-10-28 12:16:05 -0500

    working on investment project

--- Projects/Personal_Py/investment.py
+++ Projects/Personal_Py/investment.py
@@ -4,41 +4,26 @@ import random
 # AVERAGE AMOUNT OF DAYS WORKED OVER A LIFETIME IS 90,000 HOURS, OR 3715 DAYS
 
 #user enters how many days they invested for 
-def ROI(days_worked,amt_invested)
-    days_worked 
-    def standard_and_poors
-
-
-'''
-def standard_and_poors(work_days, investment_amount):
+def ROI(days_invested,amt_invested):
     counter = 1
-    avg_increase = 0.02739726027
-    total_investment = investment_amount * counter
+    while counter <= days_invested:
+        # calculate the ROI for the day
+        # daily investment of $100 = (100 * ( 1 + 0.02739726027)) - 100 ) = 2.739726027 after 1st day
+        daily_roi = amt_invested * (1 + amt_invested) - amt_invested
 
-    return_on_investment = 0
-    while counter <= work_days:
-        # Calculate fluctuation for the day
-        fluctuation = random.uniform(avg_increase * 50, avg_increase * 150)
+        # print the ROI for the day
+        print(f"\nROI on day {days} after investing ${amt_invested:.2f} is ${daily_roi:,.2f}")
 
-        # Adjust ROI based on fluctuation (apply percentage change to the current value)
-        daily_roi = investment_amount * fluctuation - investment_amount
+        profit_earned = daily_roi * days
+        print(f"\nStock Value after investing for {days} days is ${amt_invested * (1 + daily_roi) ** days:.2f}")
 
-        return_on_investment += daily_roi
+        # fix calculation for compound return on investment
+        
 
-        # Print out the ROI for the day
-        print(f"\nROI on day {counter} after investing ${investment_amount:.2f} is ${daily_roi:.2f}")
-
-        profit_earned = return_on_investment
         counter += 1
-        print(f"\nStock Value after investing for {counter - 1} days is ${total_investment + daily_roi:.2f}")
 
-    # Program prints total amount earned after investing x amount of days
-    return return_on_investment
+days = int(input("How many days did you invest for?: "))
+amt = float(input("How much $ did you invest per day?: "))
+print (ROI(days,amt))
 
-# Program prompts the user to enter how long they've been investing for
-days = int(input("\nHow many days did you invest for? "))
-amt = int(input("\nHow much did you invest per day? "))
 
-# Program passes the user parameter into the argument used by the function "work_days"
-'''
-print(f"\nROI after investing ${amt * days:.2f} after {days} days is ${standard_and_poors(days, amt):.2f}\n")
