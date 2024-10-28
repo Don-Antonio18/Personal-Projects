e5bf529f8e28edf89af20c1556a38c9ed6ae2e8b
Don-Antonio18 <antoniokerrrocks@gmail.com>
2024-10-28 12:06:43 -0500

    Update investment.py

--- Projects/Personal_Py/investment.py
+++ Projects/Personal_Py/investment.py
@@ -1,8 +1,15 @@
 import random
-
 # S & P 500 RETURNS AROUND 11% PER YEAR ON AVERAGE, WHICH IS 0.02739726027 PERCENT PER DAY
 # THIS PROGRAM CALCULATES ROI ON A $100 INVESTMENT PER DAY
 # AVERAGE AMOUNT OF DAYS WORKED OVER A LIFETIME IS 90,000 HOURS, OR 3715 DAYS
+
+#user enters how many days they invested for 
+def ROI(days_worked,amt_invested)
+    days_worked 
+    def standard_and_poors
+
+
+'''
 def standard_and_poors(work_days, investment_amount):
     counter = 1
     avg_increase = 0.02739726027
@@ -33,4 +40,5 @@ days = int(input("\nHow many days did you invest for? "))
 amt = int(input("\nHow much did you invest per day? "))
 
 # Program passes the user parameter into the argument used by the function "work_days"
+'''
 print(f"\nROI after investing ${amt * days:.2f} after {days} days is ${standard_and_poors(days, amt):.2f}\n")
