c7c59b075d094b6377952420fbf00a15090d5e0b
Don-Antonio18 <antoniokerruni@gmail.com>
2024-10-28 23:20:49 -0500

    notes on strings

--- Projects/Personal_Py/string_funcs.py
+++ Projects/Personal_Py/string_funcs.py
@@ -1,15 +1,34 @@
 name = ("Enter your name: ")
 print(help(str))
 
+#returns the number of characters in a string.
 length = len(name)
+
+# converts the first character of a string to uppercase and makes all other characters lowercase.
 size = name.capitalize()
+
+#returns True if all characters in the string are alphanumeric (either alphabets or numbers).
 alphanumeric = name.isalnum()
+
+#True if all characters in the string are alphabets.
 alphabetic = name.isalpha()
+
+# True if all characters in the string are digits.
 digit = name.isdigit()
-result = name.upper()
+
+# returns True if all characters in the string are uppercase.
 result = name.isupper()
+
+#returns True if all characters in the string are lowercase.
 result = name.islower()
+
+# converts all characters in a string to uppercase.
+result = name.upper()
+
+# The lower() function converts all characters in a string to lowercase.
 result = name.lower()
+
+#the count() function returns the number of characters that meet the condition
 
 phone_number = input("enter ur phone number: ")
 # check to see how much dashes in phone num:
