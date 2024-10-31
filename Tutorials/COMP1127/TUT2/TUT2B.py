ede541ab4d747b99e2dfef63abb6f72a352cf470
Don-Antonio18 <antoniokerruni@gmail.com>
2024-10-31 01:06:04 -0500

    claude suggestion

--- Tutorials/COMP1127/TUT2/TUT2B.py
+++ Tutorials/COMP1127/TUT2/TUT2B.py
@@ -3,33 +3,35 @@ rational numbers r1 and r2 need to be added
 r1 is a tuple of numerator
 r2 is a tuple of denominator
 
-A rational num = quotient of numerator / denominator, where b!= 0
+A rational num = quotient of numerator / denominator, where denominator != 0
 
 1. Create function naive-add that returns sum of r1 + r2
 '''
-r1 = (numer, denom)
-r2 = (numer, denom)
+
 def naive_add(r1, r2):
-    return (r1[0] * r2[1] + r2[0] * r1[1]) / r1[1] * r2[1]
+    r1 = (n1, d1)
+    n2 = (n1, d2)
+    return (n1*d2 + n2*d1) / (d1*d2)
 
-'''
-The software vendor Rational Numbers Inc. defines 
-a constructor for a rational number using
-the function make-rational below.
-'''
+''' The software vendor Rational Numbers Inc. defines 
+    a constructor for a rational number using
+    the function make-rational below. '''
+    
 def make_rational(numer, denom):
-    return [("rational", numer,denom)]
+    return ["rational", numer,denom]
 
-'''
-Complete Rational’s rational number ADT by implementing 
+''' Complete Rational’s rational number ADT by implementing 
 functions get_numer and get_denum
 that return the numerator and denominator respectively, 
-given a rational number r
-'''
+given a rational number r   ''''
 
 def get_numer(r):
-    return make_rational(numer)
+    return r[1]
 
 def get_denum(r):
+    return r[2]
 
-    return make_rational(denom)
+''' Use the rational number ADT to implement the functions rat_add, rat_subt, rat_mult and rat_div that
+implement addition, subtraction, multiplication and division of rational numbers respectively.'''
+
+def rad_add(r1, r2)
