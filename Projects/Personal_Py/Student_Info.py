1ea52245879ece9c0c3ce3674f285b565a92e1c5
Don-Antonio18 <antoniokerruni@gmail.com>
2024-10-31 12:23:47 -0500

    added pygame

---
+++ Projects/Personal_Py/Student_Info.py
@@ -0,0 +1,10 @@
+StudentProfile = { 
+    'Name': 'Antonio',
+    'Age': '20',
+    'Grade': 'Gr',
+    'Course': 'Course',
+    'Email': 'Email',
+    'Phone': 'Phone'
+}
+
+print (StudentProfile["Name"], end="")
