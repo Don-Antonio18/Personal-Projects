0512afbccf342ef01619a93285d71dda01e15b12
Don-Antonio18 <antoniokerruni@gmail.com>
2024-11-01 01:24:53 -0500

    update

--- Projects/Personal_Py/Tuple.py
+++ Projects/Personal_Py/Tuple.py
@@ -27,3 +27,20 @@ else:
 
 for i in student:
     print (i, end=" ")   
+
+''' If the number of variables is less than the number of values,  
+you can add an * to the variable name and the values will be assigned to the variable as a list:
+
+Example: Assign the rest of the values as a list called "red": '''
+
+fruits = ("apple", "banana", "cherry", "strawberry", "raspberry")
+(green, yellow, *red) = fruits
+
+print(green)
+print(yellow)
+print(red)
+
+fruits.
+''' This will Output:
+'apple', 'banana', ['cherry', 'strawberry', 'raspberry']'''
+
