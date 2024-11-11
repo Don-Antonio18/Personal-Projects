78fc47588a5ca17d9abb26c27aa38289de5acb02
Don-Antonio18 <antoniokerruni@gmail.com>
2024-11-11 13:41:35 -0500

    added nested sort of folders

--- Projects/School/HigherOrderProds.py
+++ Projects/School/HigherOrderProds.py
@@ -1,11 +1,15 @@
-def sq(n):     return n ** 2
-def incrmt(n):  return n + 1
-
 def sum(term, start, next, stop):
     if start > stop:
         return 0
     else:
         return term(start) + \
             sum(term, next(start), next, stop)
+            
+def sq(n):     return n ** 2
+def incrmt(n):  return n + 1
 
 print(sum(sq, 1, incrmt, 4))
+
+#? Using lambda functon, replace sq and incrmt functions
+            # sq funct        # incrmt func
+print(sum(lambda n: n **2, 1, lambda n: n + 1, 4  ) )
--- Projects/School/Summation.py
+++ Projects/School/Summation.py
@@ -1,5 +1,5 @@
 
-#* CAPTURING THE SUMMATION OF A SERIES of INTEGERS
+#! CAPTURING THE SUMMATION OF A SERIES of INTEGERS
 def sumIntegers(start, stop):
     if start > stop: return 0 # stops recursing & loops back thru
         # returns num + next num in series 
@@ -7,7 +7,7 @@ def sumIntegers(start, stop):
         return start + sumIntegers(start + 1, stop)
     
     
-#* CAPTURING THE SUMMATION OF SQUARES
+#! CAPTURING THE SUMMATION OF SQUARES
 def sqr(n): return n ** 2
 
 def sumSquares(start, stop):
@@ -16,8 +16,11 @@ def sumSquares(start, stop):
     else: 
         return sqr(start) + sumSquares(start + 1, stop)
 
+#? Using lambda to replace sqr function:
+#? sum(lambda n: n**2, 1, lambda x: x+ 1, 4 )
 
-#* CAPTURING THE SUMMATION OF RECIPROCALS
+
+#! CAPTURING THE SUMMATION OF RECIPROCALS
 def sqReciprocal(x): return 1/ (x * x)
 
 def piSum(start, stop):
@@ -26,4 +29,5 @@ def piSum(start, stop):
     else: 
         return sqReciprocal(start) + piSum(start + 2, stop)
 
-
+#* Using lambda to replace reciprocal function:
+#* sum(lambda n: 1.0(n * n), 1, lambda n: n + 2, 101)
