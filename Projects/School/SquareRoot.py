476889dadb773fb6cf71c8f638415ca063f54cb2
Don-Antonio18 <antoniokerruni@gmail.com>
2024-11-11 12:52:36 -0500

    optimized further

--- Projects/School/SquareRoot.py
+++ Projects/School/SquareRoot.py
@@ -1,13 +1,10 @@
 '''write square root of a given perfect square without using inbuilt functions'''
 import math
-def sq_root(x):
-    return (f"The square root of {x} is {x ** 0.5}")
+                #! x to the power of 0.5 gives square root 
+def sq_root(x): return (f"The square root of {x} is {x ** 0.5}")
 
 number = int(input("Enter a number and we will calculate its square root: "))
 print (sq_root(number))
-
-#! x to the power of 0.5 gives square root 
-def sqrt(x): return x ** 0.5 
 
 #* function that returns square of a number:
 def sqr(n): return n ** 2
