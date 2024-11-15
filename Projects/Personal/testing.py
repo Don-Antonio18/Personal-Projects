15a07ca298184f6238164e15f626dbef71e3112a
Don-Antonio18 <antoniokerruni@gmail.com>
2024-11-14 21:44:11 -0500

    check out some code from test

---
+++ Projects/Personal/testing.py
@@ -0,0 +1,15 @@
+# matrix = [[1, 2], [3, 4], [5, 6]]
+# print ([item for sublist in matrix for item in sublist])
+
+my_string = 'geeksforgeeks'
+for i in range(len(my_string)): 
+    my_string[i].upper() 
+print (my_string)
+
+# a_list = [1,2,3,4]
+# a_list.reverse()
+# print(a_list)
+
+# a = 4.5
+# b = 2
+# print (a // b)
