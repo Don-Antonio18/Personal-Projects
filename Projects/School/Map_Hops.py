a814c8088971f6a8f66b263ef4896325e1322d2c
Don-Antonio18 <antoniokerruni@gmail.com>
2024-11-18 12:55:02 -0500

    imported pytest

--- Projects/School/Map_Hops.py
+++ Projects/School/Map_Hops.py
@@ -1,8 +1,11 @@
 
-
+def my_map(f, lst):
+    if list == []:  return []
+    else:
+        return [f(lst[0])] + my_map(f, lst[1:])
 
 def sq(x): return x * x
 lst =  [3, 5, 7, 9, 11, 13]
-lst2 = map(sq, lst)
+lst2 = my_map(sq, lst)
 
-print map(lambda x: 2*x, map(sq, lst))
+print (my_map(lambda x: 2*x, map(sq, lst)))
