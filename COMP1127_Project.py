51d9f0251f30e924520fb751ac304092bf8c73de
Don-Antonio18 <antoniokerruni@gmail.com>
2024-11-20 15:55:06 -0500

    started adding proper documentation

--- COMP1127_Project.py
+++ COMP1127_Project.py
@@ -3,23 +3,30 @@ def makePacket(srcIP, dstIP, length, prt, sp,dp,sqn, pld):
     return ("PK", srcIP, dstIP, [length, prt, [sp,dp], sqn, pld] )
 
 def getPacketSrc(pkt):
-    return pkt[1]
+    """ returns srcIP frm tuple """
+    return pkt[1] 
 
 def getPacketDst(pkt):
-    return pkt[2] # returns 3rd index of tuple
+    """returns dstIP frm tuple"""
+    return pkt[2] 
 
 def getPacketDetails(pkt):
-    return pkt[3] # returns lst with packet details
+    """returns lst with packet details"""
+    return pkt[3] 
 
 def isPacket(pkt):
-    #    makes sure input has meets all conditions of packet adt
-    return isinstance(pkt, tuple) and pkt[0] == "PK" and isinstance(pkt[1], str) and isinstance(pkt[2], str) \
-            and isinstance(pkt[3], list) and len(pkt[3]) == 5 and len(pkt) == 4 
+    """makes sure input has meets all conditions of packet adt"""
+    return isinstance(pkt, tuple) \
+    and pkt[0] == "PK" \
+    and isinstance(pkt[1], str) \
+    and isinstance(pkt[2], str) \
+    and isinstance(pkt[3], list) \
+    and len(pkt[3]) == 5 \
+    and len(pkt) == 4 
     
 def isEmptyPkt(pkt):
-    return pkt == ()
+    """returns True if pkt is tuple data type & false otherwise"""
+    return pkt == () 
 
 #PART 2
 
-
-    
