2fc7706e6dd752dec5a4b88d5c2f10453169c05b
Don-Antonio18 <antoniokerruni@gmail.com>
2024-12-07 23:00:53 -0500

    Enhance BinaryTree.py: Introduce level order and pre-order tree creation examples with improved comments for better understanding.

--- py/uni/BinaryTree.py
+++ py/uni/BinaryTree.py
@@ -6,8 +6,8 @@ def makeemptytree(root):
 def maketree(root, left, right):
     return ("DT", [root, left, right])
 
-#* parent node
-t = maketree (
+#* level order binary tree
+level_order_tree = maketree (
     # el, left, right
     1, 
     #left branch
@@ -29,3 +29,71 @@ t = maketree (
     
 #end of tree
 )
+print(level_order_tree)
+
+#will try to create pre-order tree.
+#pre-order tree:
+preordertree = maketree(
+    1,                               # Root
+    maketree(
+        2,                          # Left subtree of 1
+        maketree(
+            4,                      # Left subtree of 2
+            maketree(
+                8,                  # Left subtree of 4
+                makeemptytree(None),
+                makeemptytree(None)
+            ),
+            maketree(               # Right subtree of 4
+                9,
+                makeemptytree(None),
+                makeemptytree(None)
+            )
+        ),
+        maketree(                   # Right subtree of 2
+            5,
+            maketree(               # Left subtree of 5
+                10,
+                makeemptytree(None),
+                makeemptytree(None)
+            ),
+            maketree(               # Right subtree of 5
+                11,
+                makeemptytree(None),
+                makeemptytree(None)
+            )
+        )
+    ),
+    maketree(                       # Right subtree of 1
+        3,
+        maketree(                   # Left subtree of 3
+            6,
+            maketree(               # Left subtree of 6
+                12,
+                makeemptytree(None),
+                makeemptytree(None)
+            ),
+            maketree(               # Right subtree of 6
+                13,
+                makeemptytree(None),
+                makeemptytree(None)
+            )
+        ),
+        maketree(                   # Right subtree of 3
+            7,
+            maketree(               # Left subtree of 7
+                14,
+                makeemptytree(None),
+                makeemptytree(None)
+            ),
+            maketree(               # Right subtree of 7
+                15,
+                makeemptytree(None),
+                makeemptytree(None)
+            )
+        )
+    )
+)
+    
+    
+    
